ef3c5c1cd6bb6f27a082c6c1a1e8165215c6bd81
Will Woodward <16128408+NotSampleText@users.noreply.github.com>
2022-02-21 22:52:26 -0800

    Changed file creation method
    It's still not copying the content of the input file (I think?) but I'm tired and sick so wth

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <unistd.h>
 #include <string>
 #include <cstring>
+#include <fstream>
 
 using namespace std;
 
@@ -22,6 +23,12 @@ int main(int argc, char *argv[])
         return 1;
     }
 
+    if (fopen(inputFile.c_str(), "r") == NULL)
+    {
+        printf("Input file cannot be opened.\n");
+        return 1;
+    }
+
     int numCopies = atoi(argv[2]);
 
     if (&numCopies == NULL)
@@ -36,19 +43,30 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    string fileExt = ".sus";
-
     for (int i = 0; i < numCopies; i++)
     {
         int fileSuffix = rand() % 100; // Find random number in range 0 <-> 99
 
-        string temp = inputFile + to_string(fileSuffix) + fileExt;
-        char fileName[temp.length()];
-        strcpy(fileName, temp.c_str());
+        size_t t = inputFile.find_last_of(".");
+        string outputFile;
 
-        // Create the file and close it
-        FILE *f = fopen(fileName, "wb");
-        fclose(f);
+        if (t == string::npos)
+        {
+            outputFile = inputFile + to_string(fileSuffix);
+        }
+        else
+        {
+            string stem = inputFile.substr(0, t);
+            string ext = inputFile.substr(t, inputFile.length());
+
+            outputFile = stem + to_string(fileSuffix) + ext;
+        }
+
+        ifstream src(inputFile, ios::binary);
+        ofstream dst(outputFile, ios::binary);
+
+        dst << src.rdbuf();
+
     }
     
     // 
