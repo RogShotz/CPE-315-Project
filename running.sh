77d6b02800f6ec0ff3ef5464f716396f16939e6e
Zack Soucek <zacksoucek@gmail.com>
2022-03-08 21:40:12 -0800

    file changes

--- running.sh
+++ running.sh
@@ -3,9 +3,14 @@
 #variable
 
 #clean filedump directory
-#run 500,000 copies of each file
-COPIES=50
-
+#run 1,000 copies of each file
+COPIES=1500
+#filenames array
+FILES=("sourceimages/john_my_boi_11x11.bmp sourceimages/john_my_boi_25x25.bmp sourceimages/john_my_boi_28x28.bmp sourceimages/john_my_boi_30x30.bmp sourceimages/john_my_boi_32x32.bmp sourceimages/john_my_boi_40x40.bmp sourceimages/john_my_boi_50x50.bmp sourceimages/john_my_boi_144.bmpv sourceimages/john_my_boi_240.bmp sourceimages/john_my_boi_480.bmp  sourceimages/john_my_boi_720.bmp sourceimages/john_my_boi_1920.bmp sourceimages/john_my_boi_4k.bmp")
+#outputfiles array
+OUTPUT=(./perfOutputs/*.out)
+#OUTPUT="perfOutputs/0_6kb.out,perfOutputs/2kb.out,perfOutputs/3kb.out,perfOutputs/3_5kb.out,perfOutputs/4kb.out,perfOutputs/4_5kb.out,perfOutputs/10kb.out,perfOutputs/26kb.out,perfOutputs/71kb.out,perfOutputs/285kb.out,perfOutputs/900kb.out,perfOutputs/4500kb.out,perfOutputs/18000kb.out"
+#12 total
 
 
 rm -r files
@@ -13,67 +18,10 @@ mkdir files
 make clean
 make
 
-#11x11, 600b
-time -o perfOutputs/2kb.out ./copystorm "sourceimages/john my boi_11x11.bmp" $COPIES
-rm -r files
-mkdir files
+for f in $FILES; do
 
-#25x25, 2.5kb
-time -o perfOutputs/2kb.out ./copystorm "sourceimages/john my boi_25x25.bmp" $COPIES
-rm -r files
-mkdir files
+   (time ./copystorm $f $COPIES) 3>&2 2> "perfOutputs/$COPIES/$f.out"
 
-#28x28, 3kb
-time -o perfOutputs/3kb.out ./copystorm "sourceimages/john my boi_28x28.bmp" $COPIES
-rm -r files
-mkdir files
-
-#30x30, 3.5kb
-time -o perfOutputs/3_5kb.out ./copystorm "sourceimages/john my boi_30x30.bmp" $COPIES
-rm -r files
-mkdir files
-
-#32x32, 4.13kb
-time -o perfOutputs/4kb.out ./copystorm "sourceimages/john my boi_32x32.bmp" $COPIES
-rm -r files
-mkdir files
-
-#40x40, 6.4kb
-time -o perfOutputs/4_5kb.out ./copystorm "sourceimages/john my boi_40x40.bmp" $COPIES
-rm -r files
-mkdir files
-
-#50x50, 10kb
-time -o perfOutputs/10kb.out ./copystorm "sourceimages/john my boi_50x50.bmp" $COPIES
-rm -r files
-mkdir files
-
-#144p, 26kb
-time -o perfOutputs/26kb.out ./copystorm "sourceimages/john my boi_144.bmp" $COPIES
-rm -r files
-mkdir files
-
-#240p, 71kb
-time -o perfOutputs/71kb.out ./copystorm "sourceimages/john my boi_240.bmp" $COPIES
-rm -r files
-mkdir files
-
-#480p, 285kb
-time -o perfOutputs/285kb.out ./copystorm "sourceimages/john my boi_480.bmp" $COPIES
-rm -r files
-mkdir files
-
-#720p, 900kb
-time -o perfOutputs/900kb.out ./copystorm "sourceimages/john my boi_720.bmp" $COPIES
-rm -r files
-mkdir files
-
-#1920p, 4.5MB
-time -o perfOutputs/4500kb.out ./copystorm "sourceimages/john my boi_1920.bmp" $COPIES
-rm -r files
-mkdir files
-
-#4kp, 18MB
-time -o perfOutputs/18000kb.out ./copystorm "sourceimages/john my boi_4k.bmp" $COPIES
-rm -r files
-mkdir files
+   rm -r files
+   mkdir files
+done
